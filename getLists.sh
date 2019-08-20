873b5bb29a8c384a3ef0c781cf5d77367585f4ca
asadisaghar <asadisaghar@gmail.com>
2019-08-20 07:18:08 +0000

    standardized on the cluster

--- getLists.sh
+++ getLists.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #get list of directories with dumpfiles
-for i in {1..8}
+for i in 1
 do
     echo $i
-    find /ymslanda/bandwidthestimator/data/mangleddataorwhatever/"$i"/ -name '*.npz' -printf "%h\n" > tmp"$i"
+    find /ymslanda/bandwidthestimator/data4/mangleddata/"$i"/ -name '*.npz' -printf "%h\n" > tmp"$i"
     sort tmp"$i" | uniq > lst"$i"    
 done
