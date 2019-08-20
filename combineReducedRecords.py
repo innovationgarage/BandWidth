e3244848050c4179e84a5bd475f4428c3231b38d
asadisaghar <asadisaghar@gmail.com>
2019-08-20 09:18:58 +0200

    using median instead of mode

--- combineReducedRecords.py
+++ combineReducedRecords.py
@@ -15,20 +15,21 @@ for i, filepath in enumerate(all_files):
     if i==0:
         tmp_df = pd.read_csv(filepath)
     else:
-        try:
-            tmp_df = pd.read_csv(filepath, header=0)
-            df = df.append(tmp_df)
-        except :
-            print('{} is empty!'.format(filepath))
+        tmp_df = pd.read_csv(filepath, header=0)
+        df = df.append(tmp_df)
+
+        # try:
+        # except :
+        #     print('{} is empty!'.format(filepath))
             
 df['LBW'] = pd.to_numeric(df['LBW'])
 df['nflow'] = df['filename'].apply(lambda x: int(x.split('_')[1]))
 df['flowbw'] = df['filename'].apply(lambda x: int(x.split('_')[2]))
 df['cross_trafficBW'] = (df['nflow']-1)*df['flowbw']
 df['availableBW'] = df['LBW']-df['cross_trafficBW']
-df['BW'] = df['nflow']*df['flowbw']
-df['overflow'] = df['BW']>df['LBW']
-df['l_over_gin'] = df['l_mode']/df['gin_mode']
-df['gack_over_gin'] = df['gack_mode']/df['gin_mode']
+df['overflow'] = df['MainFlowBW']+(df['nflow']-1*df['flowbw'])
+df['is_overflown'] = df['overflow'].apply(lambda x: x<0)
+df['l_over_gin'] = df['l_median']/df['gin_median']
+df['gack_over_gin'] = df['gack_median']/df['gin_median']
 
 df.to_csv('{}.csv'.format(outfile), index=False)
